fb18b0003362d9291e073884acbccdb00b402c40
Dominique Oyco <dominickoyco@gmail.com>
2020-02-24 16:38:30 -0800

    Update heapmanager.c

--- heapmanager.c
+++ heapmanager.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 const int OVERHEAD_SIZE = sizeof(struct Block);				//the size of the overhead
 
 const int VP_SIZE = sizeof(void*);							//the size of the void pointer
 
-Block *free_head;											//Global pointer to Block at head of free list
+Block *free_head;											//Global pointer that points to the first free block in the free list
 
 struct Block {
-	int block_size; 										// # of bytes in the data section
- 	struct Block *next_block; 								// pointer to the next block
+	int block_size; 										//# of bytes in the data section
+ 	struct Block *next_block; 								//pointer to the next block
 };
 // The data section follows in memory after the above struct.
 
 void my_initialize_heap(int size){
 	free_head = (Block *)malloc(size);						//use malloc to initalize the buffer of a give size
-
-	free_head->block_size = size - OVERHEAD_SIZE;			
-
-	free_head->next_block = null;
-
-	free_head->data = ((char*)free_head + OVERHEAD_SIZE);
+	free_head->block_size = size - OVERHEAD_SIZE;			//this sets the initialize size for the block size
+	free_head->next_block = null;							//sets the next block to null because 
+	free_head->data = ((char*)free_head + OVERHEAD_SIZE);	//this initializes the data pointer
 }
 
 void* my_malloc(int size){
 
 }
 
-void* my_free(void data){
 
+//Implementation of free(). Deallocates a value that was allocated in the data heap 
+//The pointer will be to the data portion of a block; moves backwards in memory to find
+//the block's overhead information, and then link it into the free list.
+void my_free(void data){
+	Block *currentBlock = (Block *)((char*)data - OVERHEAD_SIZE);
+	currBlock->next_block = free_head;
+	free_head = currBlock;
 }
 
 

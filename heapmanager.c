ad79ca699511772c7b2190e56da30add9a7a120f
Dominique Oyco <dominickoyco@gmail.com>
2020-02-24 16:00:08 -0800

    Update heapmanager.c

--- heapmanager.c
+++ heapmanager.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const int OVERHEAD_SIZE = sizeof(struct Block);				//the size of the overhead
+
+const int VP_SIZE = sizeof(void*);							//the size of the void pointer
+
+Block *free_head;											//Global pointer to Block at head of free list
+
 struct Block {
-	int block_size; 				// # of bytes in the data section
- 	struct Block *next_block; 		// pointer to the next block
+	int block_size; 										// # of bytes in the data section
+ 	struct Block *next_block; 								// pointer to the next block
 };
 // The data section follows in memory after the above struct.
 
-//overhead size
-const int struct_SIZE = sizeof(struct Block);
-
-//size of a void pointer
-const int VP_SIZE = sizeof(void*);
-
-
 void my_initialize_heap(int size){
+	free_head = (Block *)malloc(size);						//use malloc to initalize the buffer of a give size
 
+	free_head->block_size = size - OVERHEAD_SIZE;			
+
+	free_head->next_block = null;
+
+	free_head->data = ((char*)free_head + OVERHEAD_SIZE);
 }
 
 void* my_malloc(int size){

fba810e9a5790947549a495aaef1d1a39d7d5ec1
Dominique Oyco <dominickoyco@gmail.com>
2020-03-04 22:32:05 -0800

    somewhat finished main class

--- heapmanager.c
+++ heapmanager.c
@@ -1,96 +1,120 @@
 //CECS 424 ASSIGNMENT 2
-//heapmanager.c
+//main.c
 //Dominique Oyco (014605758)
-
+#include "standarddeviation.h"
 #include "heapmanager.h"
+#include <stdio.h>
+#include <math.h>
 
-// memory block
-struct Block 
-{
-	int block_size;												// # of bytes in the data section
-	struct Block *next_block; 									// pointer to the next block
-};
 
-const int OVERHEAD_SIZE = sizeof(struct Block);					//The size of the overhead
-const int VP_SIZE = sizeof(void*);								//The size of the void pointer
+//ONLY NEEDS HEX!
+int main() {
+	
+	my_initialize_heap(10000);
+	//Test 1
+	//-----------------------//
+	printf("\n***TEST 1***\n\n");
+	// int *p, *r;
+	int *p = my_malloc(sizeof(int));
+	printf("Address of int pointer: 0x%p in hex and %d in dec \n", p, p);
+	my_free(p);
+	
+	int *r = my_malloc(sizeof(int));
+	printf("Address of int pointer:  0x%p in hex and %d in dec \n", r, r);
 
-struct Block *free_head;										//global pointer that always point to the first free block in the free list.
+	//-----------------------//
+	//Test 2
+	//-----------------------//
+	printf("\n***TEST 2***\n\n");
+	int *q, *s;
+	q = my_malloc(sizeof(int));
+	s = my_malloc(sizeof(int));
+	printf("Address of int pointer 1: 0x%X in hex and %d in dec\n", q, q);
+	printf("Address of int pointer 2: 0x%X in hex and %d in dec\n", s, s);
+	printf("Size of overhead + int: %d \n", OVERHEAD_SIZE + sizeof(int));
+	//-----------------------//
+	//Test 3
+	//-----------------------//
+	printf("\n***TEST 3***\n\n");
+	int *x, *t, *v;
+	x = my_malloc(sizeof(int));
+	t = my_malloc(sizeof(int));
+	v = my_malloc(sizeof(int));
+	printf("Address of int pointer 1: 0x%X in hex and %d in dec\n", x, x);
+	printf("Address of int pointer 2: 0x%X in hex and %d in dec\n", t, t);
+	printf("Address of int pointer 3: 0x%X in hex and %d in dec\n", v, v);
+	my_free(t);
+	double *y;
+	y = my_malloc(sizeof(double));
+	printf("Address of double pointer: 0x%X in hex and %d in dec\n", y, y);
+	int *w = my_malloc(sizeof(int));
+	printf("Address of int pointer: 0x%X in hex and %d in dec\n", w, w);
+	//-----------------------//
+	//Test 4
+	//-----------------------//
+	printf("\n***TEST 4***\n\n");
+	char *c;
+	int *z;
+	c = my_malloc(sizeof(char));
+	z = my_malloc(sizeof(int));
+	printf("Address of char pointer: 0x%X in hex and %d in dec \n", c, c);
+	printf("Address of int pointer:  0x%X in hex and %d in dec \n", z, z);
+	//-----------------------//
+	
+	//Test 5
+	//-----------------------//
+	printf("\n***TEST 5***\n\n");
+	int *myArray = my_malloc(100*sizeof(int));
+	int *f = my_malloc(sizeof(int));
+	*f = 13;
+	printf("Address of array: 0x%X in hex and %d in dec \n", myArray, myArray);
+	printf("Address of int: 0x%X in hex and %d in dec \n", f, f);
+	printf("Value of int: %d in dec \n", *f);
+	my_free(myArray);
+	printf("Address of int: 0x%X in hex and %d in dec \n", f, f);
+	printf("Value of int: %d in dec \n", *f);
+	//-----------------------//
 
-/*
-uses malloc to initialize a buffer of the given size to use in your custom allocator.
-param: size - the size of the buffer
-*/
-void my_initialize_heap (int size)
-{
-	free_head = malloc(size);									//initialize the buffer of a given size in the custom allocator
-	free_head->block_size = size - OVERHEAD_SIZE; 				//initial block size
-	free_head->next_block = NULL;								//next block is set to 0
-}
 
-/*
-fills an allocation request based on the byte size 
-and returns a pointer to the data portion of the block used to satisfy the request
-param: size - the size of the buffer
-*/
-void* my_malloc (int size)
-{
-	int targetSize = ceil(size + OVERHEAD_SIZE);				//the
+	//--------------------------------------//
+	//---SOLVE STANDARD DEVIATION PROBLEM---//
+	//--------------------------------------//
 
-	//Initializing pointers
-	struct Block *currentBlock = free_head; 					//The iterator for the free head
-	struct Block *next_block;									//
-	struct Block *new_block;									//
 
-	while (currentBlock)
+	int    numberOfIntegers = 0;
+	int    currentInput = 0;
+	double stdDeviation = 0;
+
+	my_initialize_heap(10000);
+
+	//First get user input 
+
+	//Get the size of the array
+	printf("Please enter a positive integer: ");
+	scanf("%d", &numberOfIntegers);
+
+	//Allocate space for an array of entered integers
+	int *stdDevArray = my_malloc(numberOfIntegers * sizeof(int));
+
+	//Read integers into the array
+	for (int i = 0; i < numberOfIntegers; i++)
 	{
-		if (currentBlock->block_size >= targetSize)
-		{
-			int extraMem = currentBlock->block_size - targetSize;	//size of free block - size of space for allocation
-
-			//Need to determine if the extra memory can support a new block
-			if (extraMem > OVERHEAD_SIZE) 
-			{
-				//this block of code inserts a new block only if excess memory > size of
-				//the overhead
-				new_block = (struct Block *)((char *)(currentBlock + 1) + extraMem);
-				new_block->block_size = size;
-				new_block->next_block = NULL;
-
-				currentBlock = currentBlock->next_block;
-
-				return (void*)(new_block + 1);
-			}
-
-			else
-			{
-				currentBlock = currentBlock->next_block;
-
-				return (void*)(currentBlock + 1);
-			}
-
-		}
-
-		else if (currentBlock->block_size < targetSize)
-		{
-			currentBlock = currentBlock->next_block;
-		}
-
-		else
-		{
-			return NULL;
-		}
+		printf("Please enter an integer to put into the array: ");
+		scanf("%d", &stdDevArray[i]);
 	}
-}
 
-/*
-Deallocates a value allocated in the data heap by finding the block 
-with the data pointer and moving backwards in memory to find the block's
-overhead information.
-param: data - the pointer to the data portion of the block
-*/
-void my_free(void *data)
-{ 
-	struct Block *currentBlock = ((struct Block *)data - 1);					//calculating the offset of the block from the beginning of the block struct
-	currentBlock->next_block = free_head;										//The next pointer points to the head's next block
-	free_head = currentBlock;													//The pointer of the free head will be pointed to the block that is being freed
+
+	// Calculate standard deviation of integers
+	//   Formula:
+	//   sqrt( 1/n * summation( xi - u )^2, 1 to n ),
+	//   where xi = integers entered and u = total arithmetic mean
+	stdDeviation = calculateStdDeviation(stdDevArray, numberOfIntegers);
+
+	//Print the standard deviation
+	printf("The standard deviation is : %f ", stdDeviation);
+	
+
+	//system("pause");
+
+	return 0;
 }
--- main.c
+++ main.c
@@ -2,9 +2,113 @@
 //main.c
 //Dominique Oyco (014605758)
 
-#include "heapmanager.h"
 #include "standarddeviation.h"
+#include "heapmanager.h"
+#include <stdio.h>
+#include <math.h>
 
-int main(){
+void testCases(int choice){
+	my_initialize_heap(10000);
+	switch(choice){
+		case 1: 
+			printf("\nFirst Test: Allocate An Int Value\n\n");
+			int *p = my_malloc(sizeof(int));
+			int *q = my_malloc(sizeof(int));
+			printf("The address of the int pointer is: %p\n", p);
+			my_free(p);
+			printf("The address of another int pointer is: %p\n", q);
+
+		case 2:
+			printf("\nSecond Test: Allocate Two Individual Int Values\n\n");
+			int *r, *s;
+			r = my_malloc(sizeof(int));
+			s = my_malloc(sizeof(int));
+			printf("The address of the 1st int pointer is: %p\n", r);
+			printf("The address of the 2nd int pointer is: %p\n", s);
+			printf("The Size of overhead + int: %lu \n", OVERHEAD_SIZE + sizeof(int));
+
+		case 3:
+			printf("\nThird Test: Allocate An Three Int Values & a Double Value\n\n");
+			int *t, *u, *v;
+			t = my_malloc(sizeof(int));
+			u = my_malloc(sizeof(int));
+			v = my_malloc(sizeof(int));
+			printf("The address of the 1st int pointer is: %p\n", t);
+			printf("The address of the 2nd int pointer is: %p\n", u);
+			printf("The address of the 3rd int pointer is: %p\n", v);
+			my_free(u);
+			double *w;
+	    	w = my_malloc(sizeof(double));
+			printf("The address of the double pointer is: %p\n", w);
+			int *x = my_malloc(sizeof(int));
+			printf("The address of int pointer: %p\n", w);
+
+		case 4:
+			printf("\nFourth Test: Allocate An Int Pointer\n\n");
+			char *y;
+			int *z;
+			y = my_malloc(sizeof(char));
+			z = my_malloc(sizeof(int));
+			printf("The address of the char pointer: %p\n", y);
+			printf("The address of the int pointer:  %p\n", z);
+
+		case 5:
+			printf("\nFifth Test: Allocate An Int Pointer\n\n");
+			int *newArray = my_malloc(100*sizeof(int));
+			int *k = my_malloc(sizeof(int));
+			*k = 13;
+			printf("The address of the new array is: %p\n", newArray);
+			printf("Address of int: %p\n", k);
+			printf("Value of int: %d\n", *k);
+			my_free(newArray);
+			printf("Address of int: %p\n", k);
+			printf("Value of int: %d\n", *k);
+
+	}
+}
+
+void stdSolver(int *b, int n){
+	my_initialize_heap(10000);
+
+	//call the calculateStdDeviation method from the standarddeviation class
+	//to calculate the standard deviation
+	double stdDeviation = calculateStdDeviation(b, n);	
+
+	//Print the standard deviation
+	printf("The standard deviation is : %f ", stdDeviation);
+}
+
+int main() {
+  /*
+  TESTING THE HEAP MANAGER
+  */
+	int caseValue;
+	printf("Testing cases for the heap manager\n");
+	printf("Please enter a case to test (1-5): ");
+	scanf("%d", &caseValue);
+
+	testCases(caseValue);
+
+  /*
+  TESTING THE STANDARD DEVIATION PROBLEM
+  */
+	int    arrSize = 0;
+	double stdDeviation = 0;
+
+	//Get the size of the array
+	printf("Please enter the size of the array: ");
+	scanf("%d", &arrSize);
+
+	//Allocate space for an array of integers
+	int *stdDevArray = my_malloc(arrSize * sizeof(int));
+
+	//Read integers into the array
+	for (int i = 0; i < arrSize; i++)
+	{
+		printf("Please enter an integer to put into the array: ");
+		scanf("%d", &stdDevArray[i]);
+	}
+
+	stdSolver(stdDevArray, arrSize);
 	return 0;
 }
